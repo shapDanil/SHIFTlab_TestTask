34d015f4c80beba678d33d7f2d476493faf4d68e
shapDanil <11420d.shapovalov@g.nsu.ru>
2023-03-27 21:38:56 +0700

    Add laptop entity class

---
+++ src/main/java/com/cft/taskshop/model/Laptop.java
@@ -0,0 +1,43 @@
+package com.cft.taskshop.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "laptop")
+public class Laptop extends Сommodity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer laptopSizeInInches;
+
+    public Laptop(String serialNumber, String manufacturer, int price, int amount, Integer id, Integer laptopSizeInInches) {
+        super(serialNumber, manufacturer, price, amount);
+        this.id = id;
+        this.laptopSizeInInches = laptopSizeInInches;
+    }
+    public Laptop(Integer id, Integer laptopSizeInInches) {
+        this.id = id;
+        this.laptopSizeInInches = laptopSizeInInches;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getLaptopSizeInInches() {
+        return laptopSizeInInches;
+    }
+
+    public void setLaptopSizeInInches(Integer laptopSizeInInches) {
+        this.laptopSizeInInches = laptopSizeInInches;
+    }
+
+
+
+}
