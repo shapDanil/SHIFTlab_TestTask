e3e4b69b62db2c6ec272afcb19428cdb88eb4cf9
shapDanil <11420d.shapovalov@g.nsu.ru>
2023-03-28 14:00:33 +0700

    Add validation for fields of commodity

--- src/main/java/com/cft/taskshop/validation/ValidationHandler.java
+++ src/main/java/com/cft/taskshop/validation/ValidationHandler.java
@@ -1,6 +1,6 @@
 package com.cft.taskshop.validation;
 
-
+import com.cft.taskshop.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,4 +16,30 @@ public class ValidationHandler {
     public EValidationCodeResult handleAnyException(Throwable e) {
         return EValidationCodeResult.UNKNOWN_ERROR;
     }
+    public static EValidationCodeResult validateCommodity(Сommodity entityCommodity ) {
+        if (entityCommodity.getSerialNumber() == null && entityCommodity.getManufacturer() == null
+                && entityCommodity.getPrice() == 0 && entityCommodity.getAmount() == 0)
+            return EValidationCodeResult.ALL_FIELDS_ARE_EMPTY;
+
+        if (entityCommodity.getSerialNumber().equals(""))
+            return EValidationCodeResult.SERIAL_NUMBER_IS_EMPTY;
+
+        if (entityCommodity.getManufacturer() == null)
+            return EValidationCodeResult.MANUFACTURER_IS_EMPTY;
+
+        if (entityCommodity.getPrice() == 0)
+            return EValidationCodeResult.PRICE_IS_EMPTY;
+
+        if (entityCommodity.getPrice() < 0)
+            return EValidationCodeResult.PRICE_LESS_THAN_ZERO;
+
+        if (entityCommodity.getAmount() == 0)
+            return EValidationCodeResult.AMOUNT_IS_EMPTY;
+
+        if (entityCommodity.getAmount() < 0)
+            return EValidationCodeResult.AMOUNT_LESS_THAN_ZERO;
+
+        return EValidationCodeResult.NO_ERROR;
+    }
+
 }
